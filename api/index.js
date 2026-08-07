a8fb262484049123fd4738dddc6e8dcc5405f9df
Sanja <admin@missrezanna.com>
2026-08-07 05:47:40 +0530

    Set Vercel route destination to /api for api/index.js serverless function entrypoint

--- api/index.js
+++ api/index.js
@@ -1,18 +1,3 @@
-try {
-  const app = require('../backend/src/app');
-  module.exports = (req, res) => {
-    try {
-      return app(req, res);
-    } catch (err) {
-      res.statusCode = 500;
-      res.setHeader('Content-Type', 'application/json');
-      res.end(JSON.stringify({ error: err.message, stack: err.stack }));
-    }
-  };
-} catch (err) {
-  module.exports = (req, res) => {
-    res.statusCode = 500;
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({ error: 'Initialization Error: ' + err.message, stack: err.stack }));
-  };
-}
+const app = require('../backend/src/app');
+
+module.exports = app;
